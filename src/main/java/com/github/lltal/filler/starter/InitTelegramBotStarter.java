ef38356f9b05ce0aeb88dae30bbe6da05126396e
lltal <litvinov_0104@mail.ru>
2025-03-03 23:40:12 +0300

    change scan package

--- src/main/java/com/github/lltal/filler/starter/InitTelegramBotStarter.java
+++ src/main/java/com/github/lltal/filler/starter/InitTelegramBotStarter.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan({"ru.wdeath.telegram.bot.starter"})
+@ComponentScan({"com.github.lltal.filler.starter"})
 public class InitTelegramBotStarter {
     private static final Logger log = LoggerFactory.getLogger(InitTelegramBotStarter.class);
 
